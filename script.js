9677b96c286a8ce27cd1c9b5d2b286b87e8cd19a
sanjay892000 <sanjay892000@gmail.com>
2024-09-17 13:26:44 +0530

    add hearts emozi

--- script.js
+++ script.js
@@ -87,11 +87,6 @@ form.addEventListener('submit', (e) => {
   /*   let dataURL = "http://localhost:5000/api/sanjay/massage" */
   let dataURL = "https://my-portfolio-web-3083.onrender.com"
   addDataFun(dataURL);
-  document.getElementById('inputname').value=" ";
-  document.getElementById('inputemail').value=" ";
-  document.getElementById('inputsubject').value=" ";
-  document.getElementById('massage').value=" ";
-
 })
 
 let addDataFun = async (URLs) => {
@@ -109,5 +104,9 @@ let addDataFun = async (URLs) => {
   const data = await response.json();
   if (data) {
     alert('Form submitted!');
+    name=''
+    email=''
+    subject=''
+    massage=''
   }
 }
